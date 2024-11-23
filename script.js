11c4e5e09226a175c5bbaf463c1c8f5f48c58e53
MotheoMolobi <molobimotheo@gmail.com>
2024-11-23 14:33:12 +0200

    final product

--- script.js
+++ script.js
@@ -3,25 +3,31 @@ const input = document.querySelector("#input");
 const setBtn = document.querySelector("#set-btn");
 const black = document.querySelector("#black-color")
 const random = document.querySelector("#random-color")
+const eraser = document.querySelector("#eraser")
+const display = document.querySelector("#display")
+const clearBtn = document.querySelector("#clear-btn")
 
 setBtn.addEventListener("click", setGrid);
+input.addEventListener("keyup", displayText);
+clearBtn.addEventListener("click", clear);
 
-black.addEventListener("focus", () => {
-    let columns = document.getElementsByClassName("column");
+function displayText() {
+    let number = input.value
+    display.textContent = "Grid size: " + number + " x " + number;
+}
+
+gridContainer.addEventListener("mouseover", () => {
+    let columns = document.querySelectorAll(".column");
         for (let i = 0; i < columns.length; i++) {
-            columns[i].addEventListener("mouseover", colorBlack);
+            columns[i].addEventListener("mouseover", changeColor);
         }
 });
-
-random.addEventListener("focus", () => {
-    let columns = document.getElementsByClassName("column");
-        for (let i = 0; i < columns.length; i++) {
-            columns[i].addEventListener("mouseover", randomColor);
-        }
-});
-
 
 function setGrid() {
+    /** Removing all of the columns and rows in the grid **/
+    document.querySelectorAll(".column").forEach(el => el.remove());
+    document.querySelectorAll(".row").forEach(el => el.remove());
+
     let number = input.value
 
     for(let i = 0; i < number; i++) {
@@ -35,16 +41,25 @@ function setGrid() {
             row.appendChild(column);
         }
     }
-    input.value = ""
-    
+    input.value = "";
 }
 
-function colorBlack() {
-    this.style.backgroundColor = '#2e2b2b';
-}
-function randomColor() {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+function changeColor() {
+    if(black.checked) {
+        this.style.backgroundColor = '#2e2b2b';
+    }
+    else if(random.checked) {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
         this.style.backgroundColor = "#" + randomColor;
+    }
+    else if(eraser.checked) {
+        this.style.backgroundColor = 'white';
+    }
 }
 
-
+function clear() {
+    let columns = document.querySelectorAll(".column");
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].style.backgroundColor = 'white';
+    }
+}
